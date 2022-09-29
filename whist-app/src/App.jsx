710e9762793507d2a98ecb6108ecc6aa57513860
Mr. Zenjord <zenjord.inc@gmail.com>
2022-09-29 10:14:15 +0200

    Added base react app

---
+++ whist-app/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="container">
+      <div class="w-25 m-auto">
+        <img src="/icon.png" class="img-fluid" alt="logo" />
+      </div>
+      <h1 class="text-center">Welkommen til Whistklubben</h1>
+    </div>
+  )
+}
+
+export default App
