a9b1903780ff68627281b9ca82c3534966234064
Mr. Zenjord <zenjord.inc@gmail.com>
2022-10-04 21:12:36 +0200

    Removed dubble import

--- whist-app/src/main.jsx
+++ whist-app/src/main.jsx
@@ -6,8 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter } from "react-router-dom";
 
-import { BrowserRouter } from "react-router-dom";
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
